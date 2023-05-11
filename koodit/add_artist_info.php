2b7a3ecfefa16ce4553c7a919cabf46cdb2c3ebb
Juttta <n1anju00@students.oamk.fi>
2023-05-11 23:56:45 +0300

    yritystä

--- koodit/add_artist_info.php
+++ koodit/add_artist_info.php
@@ -4,6 +4,10 @@ $PDO = new PDO("mysql:host=localhost;dbname=chinook", "root", "");
 
 $body = file_get_contents("arist.json");
 $arist = json_decode($body, true);
+$album = json_decode($body, true);
+$track = json_decode($body, true);
+
+//Oli tarkoitus tehdä tähän myös albumi ja muutama kappale, mutta aika loppui kesken. 
 
 $sql = "INSERT INTO artists(name) Values('$arist[name]')";
 
